a9986d1faac01f266872cce05e9ef6c247aa903d
MayaP90 <77762815+MayaP90@users.noreply.github.com>
2021-05-03 19:01:45 -0400

    Update sleep_in_test.py

---
+++ Warmup-1/sleep_in_test.py
@@ -0,0 +1,21 @@
+'''
+Unit tests for the sleep_in function for [this Coding Bat problem](https://codingbat.com/prob/p173401)
+'''
+
+# import the code to be tested
+from sleep_in import sleep_in
+
+# begin writing test
+def describe_a_sleep_in_function():
+
+    def that_determines_whether_or_not_we_should_sleep_in():
+     '''
+      The two things we care about if it's weekend/ weekday, 
+      and if we're on vacation or not. 2 variables that can have
+      2 values each, means we have a total of (2 x 2 = 4) 4 possible
+      outcomes to be concerned about
+     ''' 
+     assert sleep_in(True, True) == True # weekday but on a vacation => sleep in
+     assert sleep_in(True, False) = False # weekday but NOT vacation => do NOT sleep in
+     assert sleep_in(True, True) = True # weekday but on vacation => sleep in
+     assert sleep_in(True, True) = True # weekday but on vacation => sleep in
